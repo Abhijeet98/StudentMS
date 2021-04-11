eea3237696c2f611814f3fa32160079d19f95c7c
Abhijeet Kawade <akawde.98@gmail.com>
2021-04-11 13:58:26 +0100

    commit

--- student_project/settings.py
+++ student_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zy%zjk!29emn_4=xr55^b%pq1w!2^%8$#%+g#w()mdc$&srzy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,4 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+#django_heroku.settings(locals())
